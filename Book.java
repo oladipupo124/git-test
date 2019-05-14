133ec7bed847f6f5ba807a48a862c67eb99b4ad6
unknown <oladipupo21@gmail.com>
2019-05-14 16:12:43 +0100

    added setter and getter to Book

--- Book.java
+++ Book.java
@@ -1,5 +1,18 @@
 public class Book {
     private int numberofPages;
-    private String nameofAutor;
-    private bolean ishardCover;
+    private String nameofAuthor;
+    private boolean ishardCover;
+
+    public void setAuthor(String author){
+        this.author = author; 
+    }
+    public void getAuthor(){
+        return author; 
+    }
+    public void setNumberofPages(int numberofPages){
+        this.numberofPages = numberofPages;
+    }
+    public int getNumberofpages(){
+        return numberofPages;
+    }
 }
